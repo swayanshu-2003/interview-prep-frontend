f929bbf649fed522eea0355080821a303bc2c7a6
Swayanshu Sekhar <swayanshupanda650@gmail.com>
2025-09-06 21:36:38 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),       // React plugin
+    tailwindcss(), // Tailwind plugin
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'), // @ -> src
     },
   },
 })
